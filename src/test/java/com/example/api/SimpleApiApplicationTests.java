efe470d9678f245b1c008a242b3d12587c6341fc
nehalm4 <nehalm4@gmail.com>
2023-05-21 12:16:46 +0530

    New Request changes

--- src/test/java/com/example/api/SimpleApiApplicationTests.java
+++ src/test/java/com/example/api/SimpleApiApplicationTests.java
@@ -1,13 +1,31 @@
 package com.example.api;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.regex.Matcher;
+
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class SimpleApiApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	void verifyHelloMessage() throws Exception {
+		String patternString = "(Hello AWS Continuous Delivery! From host: ).*\\/\\d+.\\d+.\\d+.\\d+";
+		org.hamcrest.Matcher<String> regexMatcher = Matchers.matchesRegex(patternString);
+
+		this.mockMvc.perform(get("/hello")).andExpect(status().isOk()).andExpect(content().string(regexMatcher));
 	}
 
 }
