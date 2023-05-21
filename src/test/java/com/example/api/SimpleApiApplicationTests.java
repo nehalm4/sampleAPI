768c5e44a5f76569f960ad8bfdf6c05c44209fac
nehalm4 <nehalm4@gmail.com>
2023-05-21 12:58:08 +0530

    Buildspec.yml changes

--- src/test/java/com/example/api/SimpleApiApplicationTests.java
+++ src/test/java/com/example/api/SimpleApiApplicationTests.java
@@ -4,8 +4,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.util.regex.Matcher;
-
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
